02fb026c24fa9d623edcba5a783881b03286978e
Leonardo Schueroff <leonardo1295@yahoo.com>
2021-12-08 12:16:55 -0300

    Aula 012 de python

--- aula 12.py
+++ aula 12.py
@@ -1,13 +1,20 @@
+
+#Programa para ler e identificar se um nome é popular, feio ou normal
+#condições aninhadas
+
+
 nome = str(input('Qual é o seu nome?: '))
 if nome == 'Gustavo':
     print ('Que nome bonito')
 
-elif nome == 'Pedro' or 'Maria' or nome == 'Paulo':
+elif nome == 'Pedro' or nome =='Maria' or nome == 'Paulo':
     print('Seu nome é bem popular no Brasil.')
 
-elif nome in 'Joackin Debi Saracura':
+elif nome in 'Joackin Debi Saracura Cristivaldo Osvaldo Osmarilho Cristivaldo Neymar':
     print('Seu nome é muito feio')
 
 else:
     print ('Seu nome é bem normal.')
+
+print('Tenha um bom dia {}'.format(nome))
 
