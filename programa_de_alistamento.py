26dd4b1ac3f3863d7a6120d4d22c62dbbf7ce3eb
Leonardo Schueroff <leonardo1295@yahoo.com>
2021-12-08 18:16:16 -0300

    Aula 012 de python

--- programa_de_alistamento.py
+++ programa_de_alistamento.py
@@ -1,9 +1,8 @@
 # Faça um programa que leia o ano de nascimento de um jovem e informe,
-# de acordo com sua idade>
-# - Se ele ainda vai se alistar ao serviço militar
+# de acordo com sua idade
+# - Se ele ainda se alistará ao serviço militar
 # - Se é a hora de se alistar.
 # - Se ja passou do tempo do alistamento.
-#
 # Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
 from datetime import date
