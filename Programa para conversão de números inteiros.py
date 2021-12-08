b1019c19dbe1d172c3bd10fe7ea28283ba94ee52
Leonardo Schueroff <leonardo1295@yahoo.com>
2021-12-08 16:45:09 -0300

    Aula 012 de python

--- Programa para conversão de números inteiros.py
+++ Programa para conversão de números inteiros.py
@@ -4,11 +4,21 @@
 
 
 numero = int(input('Digite um número : ' ))
-pergunta2 = int(input(''' 1 - Binário.
+pergunta2 = int(input(''' Qual conversão deseja fazer? :
+
+1 - Binário.
 2 - Octal.
 3 - Hexadecimal
-
-converter para: '''))
+ '''))
 
 if pergunta2 == 1:
-    print('O valor convertido para binário é de: {} '.format(bin.(numero)[2:]))
+    print ('Binário: {}'.format(bin(numero)[2:]))
+
+elif pergunta2 == 2:
+    print('Octal: {}'.format(oct(numero)[2:]))
+
+elif pergunta2 == 3:
+    print('Hexadecimal: {}'.format(hex(numero)[2:]))
+
+else:
+    print('Comando inválido')
