ddc451e373b1dd0153c63201c2e171b2cac41696
Leonardo Schueroff <leonardo1295@yahoo.com>
2021-12-08 12:30:47 -0300

    Aula 012 de python

--- Desafio 036.py
+++ Desafio 036.py
@@ -1,4 +1,4 @@
-# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador ee em quantos anos ele vai pagar.
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 
 #Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o emrpéstimo será negado.
 
