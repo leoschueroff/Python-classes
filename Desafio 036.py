0979ac25518e75b265c9f20dff43a4dd16001725
Leonardo Schueroff <leonardo1295@yahoo.com>
2021-12-08 12:29:55 -0300

    Aula 012 de python

---
+++ Desafio 036.py
@@ -0,0 +1,17 @@
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador ee em quantos anos ele vai pagar.
+
+#Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o emrpéstimo será negado.
+
+
+valor = float(input('Qual é o valor da casa que você deseja comprar? : '))
+salario = float(input('Qual é o seu salário mensal? : '))
+parcelas = int(input('Em quantas parcelas você deseja? : '))
+
+prestacao = valor / parcelas
+exceder = salario * 0.30
+
+if prestacao > exceder:
+    print('Infelizmente seu empréstimo não foi aprovado')
+
+else:
+    print('O valor da sua prestação é de {:.2f}'.format(prestacao))
