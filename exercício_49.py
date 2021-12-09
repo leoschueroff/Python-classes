76e630c4f25b44b25d29060b22a5994daa884126
Leonardo Schueroff <leonardo1295@yahoo.com>
2021-12-09 16:31:18 -0300

    Tabuada utilizando estrutura de repetição.

---
+++ exercício_49.py
@@ -0,0 +1,10 @@
+# Refaça o desafio 009, mostrando a tabuada de um número
+# que o usuário escolher, só que agora
+# utilizando um laço for
+
+
+
+n = int(input('Escolha um número para ver sua tabuada: '))
+for c in range (1,11):
+    print('{} X {:2} = {}'.format(n, c, n*c))
+
