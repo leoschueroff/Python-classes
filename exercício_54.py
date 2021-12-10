e2dd026098a43cc5cf17b949c107075e954be247
Leonardo Schueroff <leonardo1295@yahoo.com>
2021-12-10 15:48:22 -0300

    Programa que diz quantas pessoas são de maioridades e quantas não são.

---
+++ exercício_54.py
@@ -0,0 +1,18 @@
+# Crie um programa que leia o ano de nascimento de sete pessoas.
+# No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas
+# ja são maiores.
+
+from datetime import date
+ano_atual = date.today().year
+maior = 0
+menor = 0
+for i in range (1,8):
+    idades = int(input('Digite o ano de nascimento da pessoa número {} : '.format(i)))
+    if ano_atual - idades >= 18:
+        maior += 1
+    else:
+        menor += 1
+
+
+print(''' Maiores de idade: {} 
+  Menores de idade: {} '''.format(maior,menor))
