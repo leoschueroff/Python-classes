40b91c6241fd5f9bbd3c3aaca535bb333f078016
Leonardo Schueroff <leonardo1295@yahoo.com>
2021-12-10 15:03:41 -0300

    Programa que avisa se uma frase é um palíndromo

---
+++ exercício_53.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia uma frase qualquer e diga
+# se ela é palíndromo, desconsiderando os espaços
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1,-1,-1):
+    inverso += junto[letra]
+print('Sua frase invertida fica: {}. {}'.format(inverso,'É uma frase palíndromo' if inverso == frase else 'Não é uma frase Palíndroma'))
+
